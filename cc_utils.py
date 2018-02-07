9ef18539101dfbbf3166ee2f18ce946dc77aa660
Hassan Sajjad <hsajjad@qf.org.qa>
2018-02-07 07:46:17 +0300

    added function to calculate f score from nbest list

--- cc_utils.py
+++ cc_utils.py
@@ -25,6 +25,44 @@ def calculateScore(pred, gold):
     f = (2 * p * r) / (p + r)
     print (p, r, f)
 
+# take an nbest input file and get the best possible score
+def calculateOracleScore(pred, gold, nbest):
+    tp = 0
+    fp = 0
+    fn = 0
+
+    for key,value in pred.items():
+        if key in gold:
+            gold[key] = 1
+            tp += 1
+            pred[key] = 1
+
+    fp = len(pred)/nbest - tp ## normlalize # predicted minus gold = ones that are not in gold
+    fn = len(gold) - tp # present in gold but not in predicted
+    print (tp, fp, fn)
+     
+    
+    print (tp, fp, fn)
+    
+    p = tp / (tp + fp)
+    r = tp / (tp + fn)
+    f = (2 * p * r) / (p + r)
+    print (p, r, f)
+
+#### take nbest file and make a dictionary to calcualte oracle score
+def nbest_oracle(nbest_file, pred_nbest):
+    
+    nbest = open (nbest_file, 'r', encoding='utf-8')
+    for line in nbest:
+        line = line.rstrip()
+        fr_id, fr, en_id, en = line.split("\t")
+        fr_id = str(int(fr_id.replace('fr-','')))
+        en_id = str(int(en_id.replace('en-','')))
+        pred_nbest[fr_id + "\t" + en_id] = 0
+    
+    nbest.close()
+    return pred_nbest
+
 
 def load_gold(fname):
     fin = open (fname, 'r', encoding='utf-8')
